2b854a6863d7ff628283e8563fe739936eed7a3d
chnnick <nickchenschool@gmail.com>
2024-12-28 22:11:44 -1000

    backend API completed

--- backend/server.js
+++ backend/server.js
@@ -1,14 +1,18 @@
 import express from "express";
 import dotenv from "dotenv";
+import { connectDB } from './config/db.js';
+import productRoutes from "./routes/product.route.js";
 
 dotenv.config();
 
 const app = express();
+const PORT = process.env.PORT || 5001;
 
-app.get("/products", (req, res) => {});
+app.use(express.json());
 
-console.log(process.env.MONGO_URI);
+app.use("/api/products", productRoutes);
 
-app.listen(5001, () => {
-  console.log("Server started at http://localhost:5001");
+app.listen(PORT, () => {
+  connectDB();
+  console.log("Server started at http://localhost:" + PORT);
 });
