60b03e1550e81d59c5b3a6ec13d4a72bef96e3a6
chnnick <nickchenschool@gmail.com>
2024-12-29 14:02:09 -1000

    new proxy settings for flexible ports

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -2,12 +2,14 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import jsconfigPaths from "vite-jsconfig-paths"
 
+const PORT = process.env.PORT || 5001;
+
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), jsconfigPaths()],
   server: {
     proxy: {
-      "/api": "http://localhost:5001/",
-    },
+      "/api": `http://localhost${PORT}`
+    }
   }
 })
