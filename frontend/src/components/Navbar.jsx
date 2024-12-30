96e7e0092ab0b2e0eada28cd7add9bc06d17122b
chnnick <nickchenschool@gmail.com>
2024-12-29 23:59:56 -1000

    Unstyled navbar

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,9 +1,23 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import "./NavbarStyles.css"
 
 const Navbar = () => {
   return (
-    <div>Navbar</div>
+    <div className="navbar">
+    <p className="nav-text">Dreamcore</p>
+    <nav>
+      <ul>
+        <li>
+          <Link to="/contact">Contact</Link>
+        </li>
+        <li>
+          <Link to="/mystery">???</Link>
+        </li>
+      </ul>
+    </nav>
+    </div>
   )
 }
 
-export default Navbar
+export default Navbar;
