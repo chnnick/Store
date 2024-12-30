1e76573dc369ee3350b286c364f9cf251154f24f
Drewszoke <57788297+Drewszoke@users.noreply.github.com>
2024-12-29 22:39:14 -1000

    Feat(AddProduct): New Add Products Page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,8 @@
 import { Box } from "@chakra-ui/react";
-import { Route, Routes } from "react-router-dom";
+import { Link, Route, Routes } from "react-router-dom";
 import CreatePage from "./pages/CreatePage";
 import HomePage from "./pages/HomePage";
+import AddProductPage from "./pages/AddProductPage";
 import Navbar from "./components/Navbar";
 
 function App() {
@@ -9,9 +10,13 @@ function App() {
   return (
     <Box minH={"100vh"}>
       <Navbar />
+      <nav style={{ padding: '10px', backgroundColor: '#282c34', color: 'white' }}>
+        <Link to="/addProduct" style={{ color: 'white', textDecoration: 'none' }}>Add Product</Link>
+      </nav>
       <Routes>
         <Route path="/" element={<HomePage />} />
         <Route path="/create" element={<CreatePage />} />
+        <Route path="/addProduct" element={<AddProductPage />} />
       </Routes>
     </Box>
   )
